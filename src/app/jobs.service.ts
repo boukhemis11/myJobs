820b5cc6cd5692a8aa57813088b687b5c88fb896
boukhemis11 <boukhemis11@gmail.com>
2018-12-22 19:47:18 +0100

    observable

--- src/app/jobs.service.ts
+++ src/app/jobs.service.ts
@@ -1,25 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import { map } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { Observable, Subject, of } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobsService {
 
+  initialJobs = [];
   jobs = [];
   jobSubject = new Subject();
 
   constructor(private http: Http) {}
 
   getAllJobs() {
+    if (this.jobs.length > 0 && this.initialJobs.length > 0) {
+       console.log('case if ', this.jobs);
+    return of([...this.jobs, ...this.initialJobs]);
+    } else if (this.jobs.length > 0 && this.initialJobs.length === 0) {
+    console.log('case else if');
     return this.http.get('data/jobs.json')
-      .pipe(map(res => res.json()));
+        .pipe(map(res => res.json()))
+        .pipe(tap(data => {
+          this.initialJobs = data;
+          this.jobs = [...this.jobs, ...this.initialJobs];
+        }));
+    } else {
+    console.log('case else');
+    return this.http.get('data/jobs.json')
+            .pipe(map(res => res.json()))
+            .pipe(tap(data => this.initialJobs = data));
+    }
   }
 
   addJob(jobData) {
     jobData.id = Date.now();
+    this.jobs = [jobData, ...this.jobs];
     return this.jobSubject.next(jobData);
   }
 }
